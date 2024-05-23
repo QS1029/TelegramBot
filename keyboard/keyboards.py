c0b97f76c2426cabc6f9dbe2a1c90fd79677c515
Cybersport <Cybersport@26kadr.internal>
2024-05-23 15:47:39 +0300

    rewrote the bot responses to make him into a simple git assistant

--- keyboard/keyboards.py
+++ keyboard/keyboards.py
@@ -2,9 +2,11 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_keyboard_start1():
     keyboard_main = InlineKeyboardMarkup(row_width=1)
-    button1 = InlineKeyboardButton("Go to menu 2", callback_data = "go_to_2")
-    button2 = InlineKeyboardButton("Send me a random number (If I roll a 6 you fucking die)", callback_data="send_random_number")
-    keyboard_main.add(button1, button2)
+    button = InlineKeyboardButton("Как начать работать с гит в проекте?", callback_data = "push_tutorial")
+    button1 = InlineKeyboardButton("Не могу сделать push в указанную ветку, почему?", callback_data = "push_issue")
+    button2 = InlineKeyboardButton("Как отобразить свое имя в коммитах?", callback_data="commit_name_display")
+    button3 = InlineKeyboardButton("[Стр. 2 - ]", callback_data = "go_to_2")
+    keyboard_main.add(button, button1, button2, button3)
     return keyboard_main
 
 def get_keyboard_start2():
@@ -13,10 +15,3 @@ def get_keyboard_start2():
     button4 = InlineKeyboardButton("What time is it (im deranged i cant look it up myself)", callback_data="send_datetime")
     keyboard_main2.add(button3, button4)
     return keyboard_main2
-
-def get_keyboard_inline(): #for the first keyboard
-    keyboard_inline = InlineKeyboardMarkup(row_width=2)
-    but_inline = InlineKeyboardButton("Посмотреть", url="https://i.ytimg.com/vi/QlChyW_6kYA/maxresdefault.jpg")
-    but_inline_2 = InlineKeyboardButton("Что-то", url="https://patents.google.com/patent/US20090292143A1/en")
-    keyboard_inline.add(but_inline, but_inline_2)
-    return keyboard_inline
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
-from keyboard.keyboards import get_keyboard_start1, get_keyboard_start2, get_keyboard_inline
+from keyboard.keyboards import get_keyboard_start1, get_keyboard_start2
 from database.database import initialize_db, get_user, add_user
 import random
 from datetime import datetime
@@ -12,9 +12,9 @@ initialize_db()
 #COMMAND LIST
 async def set_commands(bot: Bot):
     commands = [
-        types.BotCommand(command = '/start', description = "star anew"),
-        types.BotCommand(command = '/help', description = "ask for assistance"),
-        types.BotCommand(command = '/lobotomy', description = "but would you win?")
+        types.BotCommand(command = '/start', description = "Вызвать меню"),
+        types.BotCommand(command = '/help', description = "Памятка о библиотеке Телеграм бота"),
+        #types.BotCommand(command = '/lobotomy', description = "but would you win?")
     ]
     await bot.set_my_commands(commands)
 
@@ -24,16 +24,31 @@ async def start(message: types.Message):
     user = get_user(message.from_user.id)
     if user is None: add_user(message.from_user.id, message.from_user.username, message.from_user.first_name,
     message.from_user.last_name)
-    await message.reply('hiiiiii welcome the main menu of this definetely good bot!!!',
+    await message.answer('Ты можешь спросить меня о коммандах git, которые часто используются пользователями,'
+                         'и часто забываются новичками.',
                         reply_markup = get_keyboard_start1())
 
-@dp.callback_query_handler(lambda c: c.data == 'send_random_number')
-async def random_number(callback_query: types.CallbackQuery):
-    await callback_query.message.answer(f"uhhhh, here you can have this {random.randint(1,100)}")
+@dp.callback_query_handler(lambda c: c.data == 'push_tutorial')
+async def push_tutorial(callback_query: types.CallbackQuery):
+    await callback_query.message.answer("Зайдите в терминал и введите данные команды:\n"
+                                        "1) git init (Инициализирует работу гит)\n"
+                                        "2) git add . (Выбирает все измененные файлы)\n"
+                                        "3) git commit -m 'Напишите сюда последние изменения'\n"
+                                        "4) git remote add названиеРемоута 'ссылка на репозиторий'\n"
+                                        "5) git push -u названиеРемоута имяВетки")
 
-@dp.callback_query_handler(lambda c: c.data == 'send_datetime')
-async def send_datetime(callback_query: types.CallbackQuery):
-    await callback_query.message.answer(f"its currenntly {datetime.now().strftime('%H:%M:%S')}")
+@dp.callback_query_handler(lambda c: c.data == 'push_issue')
+async def push_issue(callback_query: types.CallbackQuery):
+    await callback_query.message.answer("Вероятно ты создал ветку через сайт, а не через терминал.\n"
+                                           "Удали ветку на GitHub, и напиши данную команду в терминал:\n"
+                                           "git branch имяВетки (Cоздает ветку под указанным именем.")
+
+@dp.callback_query_handler(lambda c: c.data == 'commit_name_display')
+async def commit_name_display(callback_query: types.CallbackQuery):
+    await callback_query.message.answer("Нужно ввести пару простых комманд, и если понадобится, указать в них нужные вам значения.\n"
+                                           "git config --global 'user.name' 'Введите сюда свое имя, "
+                                           "оставить пустым для отображения имени вашего компьютера.\n"
+                                           "git config --global 'user.email' 'Введите сюда ваш EMail.'")
 
 @dp.callback_query_handler(lambda c: c.data == 'go_to_2')
 async def go_to_2(callback_query: types.CallbackQuery):
@@ -42,13 +57,13 @@ async def go_to_2(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'go_to_1')
 async def go_to_1(callback_query: types.CallbackQuery):
-    await callback_query.message.edit_text('Sentenced to be sealed away in keyboard 1 (Unseal yourself with that button fr)',
-                            reply_markup=get_keyboard_start1())
+    await callback_query.message.edit_text('Ты можешь спросить меня о коммандах git, которые часто используются пользователями,'
+                         'и часто забываются новичками.', reply_markup=get_keyboard_start1())
 
 #HELP
 @dp.message_handler(commands = 'help')
 async def help(message: types.Message):
-    await message.reply('how about you kindly discombobulat e')
+    await message.answer('При написании нового бота, устанавливай aiogram версии 2.22.2')
 
 #LOBOTOMY
 @dp.message_handler(commands='lobotomy ')
